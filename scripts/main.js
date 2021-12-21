7e25e3679e8a6c956b5ef0999835533546d70817
judah <87juda@gmail.com>
2021-12-21 16:21:40 -0300

    Basic DOM Structure applied

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,16 @@
-let div = document.querySelector('#name');
-console.log(div.classList.contains('name'));
+const menuHamb = document.querySelector(".menu-hamb");
+const menuX = document.querySelector(".menu-x");
+const menuMobileList = document.querySelector(".menu-mobile-list");
+const menuMobileBg = document.querySelector(".menu-mobile-bg");
+const everythingButMenuMobile = document.querySelectorAll(".main");
+
+function menuMobileShow(){
+  menuMobileList.classList.add(".menu-mobile-show");
+}
+
+function menuMobileHide(){
+  menuMobileList.classList.remove(".menu-mobile-show");
+}
+
+menuMobileHambBtn.addEventListener("click", menuMobileShow);
+menuMobileHambBtn.addEventListener("click", menuMobileHide);
