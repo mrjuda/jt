c0c4996dc702efc340b0da26ecc53f57f35bbcdc
judah <87juda@gmail.com>
2021-12-22 10:07:56 -0300

    Fix lints typos on css and js

--- scripts/main.js
+++ scripts/main.js
@@ -7,21 +7,6 @@ const aboutBtn = document.querySelector('.about-btn');
 const contactBtn = document.querySelector('.contact-btn');
 const main = document.querySelectorAll('.main');
 
-// const portfolio = document.querySelectorAll('.portfolio');
-// const about = document.querySelectorAll('.about');
-// const menuMobileList = document.querySelector('.menu-mobile-list');
-// const menuMobileBg = document.querySelector('.menu-mobile-bg');
-// const nameLogo = document.querySelectorAll('.name-logo');
-// const contact = document.querySelectorAll('.contact');
-// function menuMobileShow(){
-//   menuMobileList.classList.add('.menu-mobile-show');
-// }
-// function menuMobileHide(){
-//   menuMobileList.classList.remove('.menu-mobile-show');
-// }
-// menuMobileHambBtn.addEventListener('click', menuMobileShow);
-// menuMobileHambBtn.addEventListener('click', menuMobileHide);
-
 menuHamb.addEventListener('click', () => {
   menuMobileContainer.style.display = 'flex';
   menuHamb.style.display = 'none';
@@ -37,23 +22,19 @@ menuX.addEventListener('click', () => {
   menuMobileBg.style.display = 'none';
 });
 
-portfolioBtn.addEventListener('click', () => {
-  menuMobileContainer.style.display = 'none';
-  menuHamb.style.display = 'flex';
-  menuX.style.display = 'none';
-  menuMobileBg.style.display = 'none';
-});
 
-aboutBtn.addEventListener('click', () => {
-  menuMobileContainer.style.display = 'none';
-  menuHamb.style.display = 'flex';
-  menuX.style.display = 'none';
-  menuMobileBg.style.display = 'none';
-});
 
-contactBtn.addEventListener('click', () => {
-  menuMobileContainer.style.display = 'none';
-  menuHamb.style.display = 'flex';
-  menuX.style.display = 'none';
-  menuMobileBg.style.display = 'none';
-});
+// const portfolio = document.querySelectorAll('.portfolio');
+// const about = document.querySelectorAll('.about');
+// const menuMobileList = document.querySelector('.menu-mobile-list');
+// const menuMobileBg = document.querySelector('.menu-mobile-bg');
+// const nameLogo = document.querySelectorAll('.name-logo');
+// const contact = document.querySelectorAll('.contact');
+// function menuMobileShow(){
+//   menuMobileList.classList.add('.menu-mobile-show');
+// }
+// function menuMobileHide(){
+//   menuMobileList.classList.remove('.menu-mobile-show');
+// }
+// menuMobileHambBtn.addEventListener('click', menuMobileShow);
+// menuMobileHambBtn.addEventListener('click', menuMobileHide);
