c42b93da403a0936a678aff9fe3fb3d68f4cc7cc
judah <87juda@gmail.com>
2021-12-22 11:31:32 -0300

    Fix lint typos on main.js file

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,25 @@
-const menuHamb = document.querySelector(".menu-hamb");
-const menuX = document.querySelector(".menu-x");
-const menuMobileContainer = document.querySelector(".menu-mobile-container");
-const menuMobileBg = document.querySelector(".menu-mobile-bg");
-const portfolioBtn = document.querySelector(".portfolio-btn");
-const aboutBtn = document.querySelector(".about-btn");
-const contactBtn = document.querySelector(".contact-btn");
-const main = document.querySelectorAll(".main");
+const menuHamb = document.querySelector('.menu-hamb');
+const menuX = document.querySelector('.menu-x');
+const menuMobileContainer = document.querySelector('.menu-mobile-container');
+const menuMobileBg = document.querySelector('.menu-mobile-bg');
+const portfolioBtn = document.querySelector('.portfolio-btn');
+const aboutBtn = document.querySelector('.about-btn');
+const contactBtn = document.querySelector('.contact-btn');
+const main = document.querySelectorAll('.main');
 
-menuHamb.addEventListener("click", () => {
-  menuMobileContainer.style.display = "flex";
-  menuHamb.style.display = "none";
-  menuX.style.display = "flex";
-  menuMobileBg.style.display = "flex";
-  main.classList.add(".blur");
+menuHamb.addEventListener('click', () => {
+  menuMobileContainer.style.display = 'flex';
+  menuHamb.style.display = 'none';
+  menuX.style.display = 'flex';
+  menuMobileBg.style.display = 'flex';
+  main.classList.add('.blur');
 });
 
-menuX.addEventListener("click", () => {
-  menuMobileContainer.style.display = "none";
-  menuHamb.style.display = "flex";
-  menuX.style.display = "none";
-  menuMobileBg.style.display = "none";
+menuX.addEventListener('click', () => {
+  menuMobileContainer.style.display = 'none';
+  menuHamb.style.display = 'flex';
+  menuX.style.display = 'none';
+  menuMobileBg.style.display = 'none';
 });
 
 portfolioBtn.addEventListener('click', () => {
