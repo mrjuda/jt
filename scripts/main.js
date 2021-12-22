c8d0b8eb8ed8eadbbb7758654e6c75a58988b414
judah <87juda@gmail.com>
2021-12-22 10:51:35 -0300

    Add basic DOM structure to the main.js file

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+const menuHamb = document.querySelector('.menu-hamb');
+const menuX = document.querySelector('.menu-x');
+const menuMobileContainer = document.querySelector('.menu-mobile-container');
+const menuMobileBg = document.querySelector('.menu-mobile-bg');
+const portfolioBtn = document.querySelector('.portfolio-btn');
+const aboutBtn = document.querySelector('.about-btn');
+const contactBtn = document.querySelector('.contact-btn');
+const main = document.querySelectorAll('.main');
+
+
+
+// const portfolio = document.querySelectorAll('.portfolio');
+// const about = document.querySelectorAll('.about');
+// const menuMobileList = document.querySelector('.menu-mobile-list');
+// const menuMobileBg = document.querySelector('.menu-mobile-bg');
+// const nameLogo = document.querySelectorAll('.name-logo');
+// const contact = document.querySelectorAll('.contact');
+// function menuMobileShow(){
+//   menuMobileList.classList.add('.menu-mobile-show');
+// }
+// function menuMobileHide(){
+//   menuMobileList.classList.remove('.menu-mobile-show');
+// }
+// menuMobileHambBtn.addEventListener('click', menuMobileShow);
+// menuMobileHambBtn.addEventListener('click', menuMobileHide);
