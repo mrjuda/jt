6d25db300c9276e9ddf52d46dfada09383ac1ee3
judah <87juda@gmail.com>
2021-12-22 16:03:26 -0300

    Add blur to the background when mobile-menu pops out

--- scripts/main.js
+++ scripts/main.js
@@ -53,18 +53,3 @@ contactBtn.addEventListener('click', () => {
   menuX.style.display = 'none';
   menuMobileBg.style.display = 'none';
 });
-
-// const portfolio = document.querySelectorAll('.portfolio');
-// const about = document.querySelectorAll('.about');
-// const menuMobileList = document.querySelector('.menu-mobile-list');
-// const menuMobileBg = document.querySelector('.menu-mobile-bg');
-// const nameLogo = document.querySelectorAll('.name-logo');
-// const contact = document.querySelectorAll('.contact');
-// function menuMobileShow(){
-//   menuMobileList.classList.add('.menu-mobile-show');
-// }
-// function menuMobileHide(){
-//   menuMobileList.classList.remove('.menu-mobile-show');
-// }
-// menuMobileHambBtn.addEventListener('click', menuMobileShow);
-// menuMobileHambBtn.addEventListener('click', menuMobileHide);
