8c8768fe7925f3f4d0da849096420cead836de59
judah <87juda@gmail.com>
2021-12-22 09:55:54 -0300

    Fix lints typos on css and js

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,28 @@
-const menuHamb = document.querySelector(".menu-hamb"); //hambuger menu
-const menuX = document.querySelector(".menu-x"); //closing icon
-const menuMobileContainer = document.querySelector('.menu-mobile-container') //mobil menu
-const menuMobileBg = document.querySelector(".menu-mobile-bg");
-const portfolioBtn = document.querySelector(".portfolio-btn")
-const aboutBtn = document.querySelector(".about-btn")
-const contactBtn = document.querySelector(".contact-btn")
+const menuHamb = document.querySelector('.menu-hamb');
+const menuX = document.querySelector('.menu-x');
+const menuMobileContainer = document.querySelector('.menu-mobile-container');
+const menuMobileBg = document.querySelector('.menu-mobile-bg');
+const portfolioBtn = document.querySelector('.portfolio-btn');
+const aboutBtn = document.querySelector('.about-btn');
+const contactBtn = document.querySelector('.contact-btn');
 
-
-
-// const menuMobileList = document.querySelector(".menu-mobile-list");
-// const menuMobileBg = document.querySelector(".menu-mobile-bg");
-// const nameLogo = document.querySelectorAll(".name-logo");
-const main = document.querySelectorAll(".main");
-const portfolio = document.querySelectorAll(".portfolio");
-const about = document.querySelectorAll(".about");
-const contact = document.querySelectorAll(".contact");
-
+//const main = document.querySelectorAll('.main');
+//const portfolio = document.querySelectorAll('.portfolio');
+//const about = document.querySelectorAll('.about');
+// const menuMobileList = document.querySelector('.menu-mobile-list');
+// const menuMobileBg = document.querySelector('.menu-mobile-bg');
+// const nameLogo = document.querySelectorAll('.name-logo');
+// const contact = document.querySelectorAll('.contact');
 // function menuMobileShow(){
-//   menuMobileList.classList.add(".menu-mobile-show");
+//   menuMobileList.classList.add('.menu-mobile-show');
 // }
-
 // function menuMobileHide(){
-//   menuMobileList.classList.remove(".menu-mobile-show");
+//   menuMobileList.classList.remove('.menu-mobile-show');
 // }
+// menuMobileHambBtn.addEventListener('click', menuMobileShow);
+// menuMobileHambBtn.addEventListener('click', menuMobileHide);
 
-// menuMobileHambBtn.addEventListener("click", menuMobileShow);
-// menuMobileHambBtn.addEventListener("click", menuMobileHide);
-
-
-menuHamb.addEventListener('click', ()=>{
+menuHamb.addEventListener('click', () => {
   menuMobileContainer.style.display = 'flex';
   menuHamb.style.display = 'none';
   menuX.style.display = 'flex';
@@ -36,28 +30,28 @@ menuHamb.addEventListener('click', ()=>{
   main.classList.add('.blur');
 });
 
-menuX.addEventListener('click', ()=>{
+menuX.addEventListener('click', () => {
   menuMobileContainer.style.display = 'none';
   menuHamb.style.display = 'flex';
   menuX.style.display = 'none';
   menuMobileBg.style.display = 'none';
 });
 
-portfolioBtn.addEventListener('click', ()=>{
+portfolioBtn.addEventListener('click', () => {
   menuMobileContainer.style.display = 'none';
   menuHamb.style.display = 'flex';
   menuX.style.display = 'none';
   menuMobileBg.style.display = 'none';
 });
 
-aboutBtn.addEventListener('click', ()=>{
+aboutBtn.addEventListener('click', () => {
   menuMobileContainer.style.display = 'none';
   menuHamb.style.display = 'flex';
   menuX.style.display = 'none';
   menuMobileBg.style.display = 'none';
 });
 
-contactBtn.addEventListener('click', ()=>{
+contactBtn.addEventListener('click', () => {
   menuMobileContainer.style.display = 'none';
   menuHamb.style.display = 'flex';
   menuX.style.display = 'none';
